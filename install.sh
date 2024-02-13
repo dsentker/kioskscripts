65566a730e821b67af38a51898e9aef71addde14
dsentker <tracid2008@googlemail.com>
2024-02-13 15:48:00 +0100

    create dir

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ zipfile="$tag.zip"
 dir="kioskscripts-$tag/"
 kioskHome="$HOME/kiosk/"
 rm "$zipfile" && rm -r "$dir" && rm -rf "$kioskHome"
+mkdir "$kioskHome"
 wget "https://github.com/dsentker/kioskscripts/archive/$tag.zip" && unzip "$zipfile" && mv -v "$dir"* "$kioskHome"* && cd "$kioskHome" && ./prepare.sh

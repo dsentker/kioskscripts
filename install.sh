456a8ca18925c5d7e03e1b8393b28ef4f08e0b2b
dsentker <tracid2008@googlemail.com>
2024-02-13 14:01:41 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 tag="main"
-zipfile="https://github.com/dsentker/kioskscripts/archive/refs/heads/$tag.zip"
-mkdir kiosk && cd kiosk || exit
-curl -O $zipfile && unzip "kioskscripts-$tag.zip" && chmod +x setup.sh && ./setup.sh
+zipfile="https://github.com/dsentker/kioskscripts/archive/$tag.zip"
+wget $zipfile && unzip "$tag.zip" && cd "kioskscripts-$tag/" && ./setup.sh

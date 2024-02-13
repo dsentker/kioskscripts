fbe1c617fffa02935129d04b17eba46db7d1b464
dsentker <tracid2008@googlemail.com>
2024-02-13 15:49:41 +0100

    Fix typo

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ dir="kioskscripts-$tag/"
 kioskHome="$HOME/kiosk/"
 rm "$zipfile" && rm -r "$dir" && rm -rf "$kioskHome"
 mkdir "$kioskHome"
-wget "https://github.com/dsentker/kioskscripts/archive/$tag.zip" && unzip "$zipfile" && mv -v "$dir"* "$kioskHome"* && cd "$kioskHome" && ./prepare.sh
+wget "https://github.com/dsentker/kioskscripts/archive/$tag.zip" && unzip "$zipfile" && mv -v "$dir"* "$kioskHome" && cd "$kioskHome" && ./prepare.sh

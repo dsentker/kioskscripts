60c9f24d3fbb31e40fce26cc9ce3ab54749104d4
dsentker <tracid2008@googlemail.com>
2024-02-13 13:47:47 +0100

    Fix zip ball name

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 zipfile=https://github.com/dsentker/kioskscripts/archive/refs/heads/main.zip
-curl -O $zipfile && unzip $zipfile && chmod +x setup.sh && ./setup.sh
+curl -O $zipfile && unzip main.zip && chmod +x setup.sh && ./setup.sh

8d650cf00717e2e4987566e0bbe5d5aaa4a9aa76
dsentker <tracid2008@googlemail.com>
2024-02-14 17:04:12 +0100

    Fix keyboard shortcuts

--- prepare.sh
+++ prepare.sh
@@ -1,55 +1,81 @@
 #!/bin/bash
 kioskHome="$HOME/kiosk"
 
-# Setup cronjob
-runner="$kioskHome/runner.sh"
+function create_cronjob() {
+  runner="$kioskHome/runner.sh"
 
-# Check if the cronjob entry already exists
-if ! crontab -l | grep -q "$runner"; then
-    (crontab -l ; echo "* * * * * $runner") | crontab -
-    echo "Cronjob added successfully."
-else
-    # If it already exists, do nothing
-    echo "Cronjob already exists. Doing nothing."
-fi
+  # Check if the cronjob entry already exists
+  if ! crontab -l | grep -q "$runner"; then
+      (crontab -l ; echo "* * * * * $runner") | crontab -
+      echo "Cronjob added successfully."
+  else
+      # If it already exists, do nothing
+      echo "Cronjob already exists. Doing nothing."
+  fi
+}
 
-echo "Install Keyboard shortcut..."
-sudo apt install -q -y xbindkeys
-setup="$kioskHome/setup.sh"
-echo "\"lxterminal -e $setup\"" > ~/.xbindkeysrc
-echo "  Alt + k" >> ~/.xbindkeysrc
-xbindkeys --poll-rc && killall xbindkeys
-xbindkeys
-echo "Done."
+function create_keyboard_shortcuts {
+  # Keyboard shortcut
+  SETUP_SCRIPT="$kioskHome/setup.sh"
+  CONFIG_FILE="$HOME/.config/wayfire.ini"
+  SEARCH_STRING="binding_kiosksetup"
 
-# Setup wallpaper
-pcmanfm --set-wallpaper "$kioskHome"/bg.jpg
-echo "Wallpaper installed successfully."
+  # Check if the binding_kiosksetup or command_kiosksetup entry already exists
+  if ! grep -q "$SEARCH_STRING" "$CONFIG_FILE"; then
+      # Use awk to add the lines after the [command] section
+      awk -v RS= -v ORS="\n\n" -v setup_script="$SETUP_SCRIPT" '
+          /\[command\]/ {
+              print $0 "\nbinding_kiosksetup=<alt> KEY_K\ncommand_kiosksetup=lxterminal -e " setup_script
+              next
+          }
+          { print }
+      ' "$CONFIG_FILE" > tmpfile && mv tmpfile "$CONFIG_FILE"
+      echo "Entries added successfully."
+  else
+      echo "Entries already exist."
+  fi
+  echo "Keyboard shortcuts created."
+}
 
-# Hide trash icon
-config_file="$HOME/.config/pcmanfm/LXDE-pi/desktop-items-0.conf"
+function create_wallpaper {
+  pcmanfm --set-wallpaper "$kioskHome"/bg.jpg
+  pcmanfm --wallpaper-mode=fit
+  echo "Wallpaper installed successfully."
 
-# Check if the file exists
-if [ -f "$config_file" ]; then
-    # Replace the line "show_trash=1" with "show_trash=0"
-    sed -i 's/show_trash=1/show_trash=0/' "$config_file"
-    echo "Trash icon hidden."
-else
-    echo "Error: Configuration file not found at $config_file"
-fi
+  # Hide trash icon
+  config_file="$HOME/.config/pcmanfm/LXDE-pi/desktop-items-0.conf"
 
-# Ask for URL
-read -p "Specify URL for kiosk mode: (Press ENTER for default): " userInput
+  # Check if the file exists
+  if [ -f "$config_file" ]; then
+      # Replace the line "show_trash=1" with "show_trash=0"
+      sed -i 's/show_trash=1/show_trash=0/' "$config_file"
+      echo "Trash icon hidden."
+  else
+      echo "Error: Configuration file not found at $config_file"
+  fi
 
-# Use default URL if the user input is empty
-deviceId=$("$HOME"/kiosk/getid.sh)
-url=${userInput:-"https://www.google.com?q=$deviceId"} # TODO
+  pcmanfm --reconfigure
+}
 
-# Validate the URL format using a simple regex
-url_regex='^https?://[^\s/$.?#].[^\s]*$'
-if [[ ! $url =~ $url_regex ]]; then
-    echo "Invalid URL detected!"
-    # exit 1
-fi
+function create_url {
+  # Ask for URL
+  read -p "Specify URL for kiosk mode: (Press ENTER for default): " userInput
 
-echo "$url" > "$HOME"/kiosk/url.txt && echo "URL set to $url"
+  # Use default URL if the user input is empty
+  deviceId=$("$HOME"/kiosk/getid.sh)
+  url=${userInput:-"https://www.google.com?q=$deviceId"} # TODO
+
+  # Validate the URL format using a simple regex
+  url_regex='^https?://[^\s/$.?#].[^\s]*$'
+  if [[ ! $url =~ $url_regex ]]; then
+      echo "Invalid URL detected!"
+      # exit 1
+  fi
+
+  echo "$url" > "$HOME"/kiosk/url.txt && echo "URL set to $url"
+}
+
+create_cronjob;
+create_keyboard_shortcuts;
+create_wallpaper;
+create_url;
